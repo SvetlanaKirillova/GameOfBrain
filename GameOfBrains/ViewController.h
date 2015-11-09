5c9b332b3de58e34a78de19f2289c5088b22bbff
Student <student@iMac-user-20.local>
2015-11-10 03:02:26 +0600

    Initial Commit

---
+++ GameOfBrains/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GameOfBrains
+//
+//  Created by Student on 10.11.15.
+//  Copyright (c) 2015 Student. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
